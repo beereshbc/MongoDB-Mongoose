e73d7a89e7738666b7beecebe1aa08da71416a19
beereshbc <bcbeereshkumar@gmail.com>
2025-02-01 23:23:57 +0530

    comment to mongo.js file

--- mongo.js
+++ mongo.js
@@ -1,27 +1,33 @@
-const MongoClient = require('mongodb').MongoClient;
+// Edu classical 'MongoDB' language mele create madiru database
+//Only for Example and refrence
+const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  'mongodb+srv://manu:KyOP1JrHoErqQILt@cluster0-g8eu9.mongodb.net/products_test?retryWrites=true&w=majority';
+  "mongodb+srv://manu:KyOP1JrHoErqQILt@cluster0-g8eu9.mongodb.net/products_test?retryWrites=true&w=majority";
 
+// edu post action edunna app,js ge export madethi but edralli edu add agilla
+//  bcz mongoose mele API create madi connect madini
 const createProduct = async (req, res, next) => {
   const newProduct = {
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
   };
   const client = new MongoClient(url);
 
   try {
     await client.connect();
     const db = client.db();
-    const result = db.collection('products').insertOne(newProduct);
+    const result = db.collection("products").insertOne(newProduct);
   } catch (error) {
-    return res.json({message: 'Could not store data.'});
-  };
+    return res.json({ message: "Could not store data." });
+  }
   client.close();
 
   res.json(newProduct);
 };
 
+// edu get action edunna app,js ge export madethi but edralli edu work agalla
+//  bcz mongoose mele API create madi connect madini
 const getProducts = async (req, res, next) => {
   const client = new MongoClient(url);
 
@@ -30,10 +36,10 @@ const getProducts = async (req, res, next) => {
   try {
     await client.connect();
     const db = client.db();
-    products = await db.collection('products').find().toArray();
+    products = await db.collection("products").find().toArray();
   } catch (error) {
-    return res.json({message: 'Could not retrieve products.'});
-  };
+    return res.json({ message: "Could not retrieve products." });
+  }
   client.close();
 
   res.json(products);
